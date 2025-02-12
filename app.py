6337a8fcc33bb9bb300fced46472ea8eab3a9ff8
Ken <kmacallaghan@gmail.com>
2025-02-12 12:27:26 -0500

    Update app.py
    fixed linked lists

--- app.py
+++ app.py
@@ -4,21 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '''
-  <h1>Adopt a Pet!</h1>
-  <p>Browse through the links below to find your new furry friend:</p>
-  <ul> 
-  <li><a href ="/animals/dogs"Dogs</li>
-  <li><a href ="/animals/cats"Cats</li>
-  <li><a href ="/animals/rabbits"</li>
-  <li>Dogs</li>
-  <li>Cats</li>
-  <li>Rabbits</li>
-  </ul>
-  '''
+    return '''
+    <h1>Adopt a Pet!</h1>
+    <p>Browse through the links below to find your new furry friend:</p>
+    <ul> 
+        <li><a href="/animals/dogs">Dogs</a></li>
+        <li><a href="/animals/cats">Cats</a></li>
+        <li><a href="/animals/rabbits">Rabbits</a></li>
+    </ul>
+    '''
+
 @app.route('/animals/<pet_type>')
 def animals(pet_type):
-  html = f"<h1>List of Pets: {pet_type.capitalize()}</h1>"
-  return html 
-  
-
+    return f"<h1>List of Pets: {pet_type.capitalize()}</h1>"
