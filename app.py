abc4b6e88e64b0745aeea2cc9ac90f1f3cb429dc
Ken <kmacallaghan@gmail.com>
2025-02-25 19:15:21 -0500

    Update app.py
    added new route and function

--- app.py
+++ app.py
@@ -25,3 +25,8 @@ def animals(pet_type):
 
     html += "</ul>"
     return html
+
+@app.route('/animals/<pet_type>/<int:pet_id>')
+def pet(pet_type, pet_id):
+  pet = pets[pet_type][pet_id]
+  print(pet)
